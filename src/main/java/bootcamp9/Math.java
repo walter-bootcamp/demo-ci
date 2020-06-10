1cb79b5e59bb98945dedeaa731a114fa61b89676
walter-bootcamp <abc@example.comwalter.fabian98+walter-bootcamp@gmail.com>
2020-06-10 15:18:48 -0400

    Se agregaron los metodos sumaStream, producStream, contadorStream a la clase Math

--- src/main/java/bootcamp9/Math.java
+++ src/main/java/bootcamp9/Math.java
@@ -1,8 +1,14 @@
 package bootcamp9;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
 public class Math {
 	/***
-	 * Suma de 2 enteros 
+	 * Suma de 2 enteros
+	 * 
 	 * @param x
 	 * @param y
 	 * @return resultado como entero
@@ -10,8 +16,10 @@ public class Math {
 	public static int sum(int x, int y) {
 		return x + y;
 	}
+
 	/***
-	 * Division de 2 enteros 
+	 * Division de 2 enteros
+	 * 
 	 * @param x
 	 * @param y
 	 * @return resultado como entero
@@ -20,4 +28,34 @@ public class Math {
 		return (int) x / y;
 	}
 
+	/***
+	 * Suma de enteros utilizando Stream
+	 * 
+	 * @param x
+	 * @param y
+	 * @return resultado como entero
+	 */
+	public static int sumStream(List<Integer> lista) {
+		return lista.stream().reduce(0, (a, b) -> a + b);
+	}
+	/***
+	 * Producto de enteros utilizando Stream
+	 * 
+	 * @param x
+	 * @param y
+	 * @return resultado como entero
+	 */
+	public static int producStream(List<Integer> lista) {
+		return lista.stream().reduce(1, (a, b) -> a * b);
+	}
+	/***
+	 * Cantidad items en la lista
+	 * 
+	 * @param x
+	 * @param y
+	 * @return resultado como entero
+	 */
+	public static int contadorStream(List<Integer> lista) {
+		return (int) lista.stream().count();
+	}
 }
--- src/test/java/bootcamp9/MathTest.java
+++ src/test/java/bootcamp9/MathTest.java
@@ -2,16 +2,34 @@ package bootcamp9;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.*;
+
 import org.junit.jupiter.api.Test;
 
 class MathTest {
+	private List<Integer> _lista = Arrays.asList(10, 10, 5);
 
 	@Test
 	void testSum() {
 		assertEquals(2, Math.sum(1, 1));
 	}
+
 	@Test
 	void testDivide() {
 		assertEquals(5, Math.divide(10, 2));
+	}
+
+	@Test
+	void testSumaStream() {
+		assertEquals(25, Math.sumStream(_lista));
+	}
+
+	@Test
+	void testProductoStream() {
+		assertEquals(500, Math.producStream(_lista));
+	}
+	@Test
+	void testContadorStream() {
+		assertEquals(3, Math.contadorStream(_lista));
 	}
 }
