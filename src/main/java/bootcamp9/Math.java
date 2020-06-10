ea30cd8f6c6d1b1495c74000a9bd15294e7b7c54
walter-bootcamp <abc@example.comwalter.fabian98+walter-bootcamp@gmail.com>
2020-06-10 14:22:38 -0400

    First vesion of the project

---
+++ src/main/java/bootcamp9/Math.java
@@ -0,0 +1,14 @@
+package bootcamp9;
+
+public class Math {
+	/***
+	 * Suma de 2 enteros 
+	 * @param x
+	 * @param y
+	 * @return resultado como entero
+	 */
+	public static int sum(int x, int y) {
+		return x + y;
+	}
+
+}
---
+++ src/test/java/bootcamp9/MathTest.java
@@ -0,0 +1,14 @@
+package bootcamp9;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathTest {
+
+	@Test
+	void testSum() {
+		assertEquals(3, Math.sum(1, 1));
+	}
+
+}
